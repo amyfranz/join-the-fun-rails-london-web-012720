89d4eca874d923e093c5c3fc2b0918b28a7e577f
Amy Franz <amy.franz@gmail.com>
2020-02-25 11:32:31 +0000

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.references :passenger, foreign_key: true
+      t.references :taxi, foreign_key: true
       t.timestamps null: false
     end
   end
